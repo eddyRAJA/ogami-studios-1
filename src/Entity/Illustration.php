58b5557b70dd366b6db561e9865cfbe6b13124a7
amine-haddad <seniortouco@outlook.fr>
2021-11-20 04:05:04 +0100

    ajout des fixtures illustration et de la page home en preparation

---
+++ src/Entity/Illustration.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\IllustrationRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=IllustrationRepository::class)
+ */
+class Illustration
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $url;
+
+
+    /**
+     * @ORM\Column(type="datetime_immutable")
+     */
+    private $updated_At;
+
+    /**
+     * @ORM\Column(type="datetime_immutable")
+     */
+    private $created_At;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getUrl(): ?string
+    {
+        return $this->url;
+    }
+
+    public function setUrl(string $url): self
+    {
+        $this->url = $url;
+
+        return $this;
+    }
+
+    
+
+    public function getUpdatedAt(): ?\DateTimeImmutable
+    {
+        return $this->updated_At;
+    }
+
+    public function setUpdatedAt(\DateTimeImmutable $updated_At): self
+    {
+        $this->updated_At = $updated_At;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->created_At;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $created_At): self
+    {
+        $this->created_At = $created_At;
+
+        return $this;
+    }
+}
